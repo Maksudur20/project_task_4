4f7c66bb9541f3d7af32af9e020fb3c13583f79c
Maksudur20 <20maksudur00@gmail.com>
2025-02-04 16:35:15 +0600

    v2

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ const playIcon = document.getElementById("play-icon");
 function toggleMusic() {
   if (audio.paused) {
     audio.play();
-    playIcon.src = "./img/volume_off.png";
+    playIcon.src = "./img/volume_up.png";
     playIcon.style.transform = "rotate(360deg)";
   } else {
     audio.pause();
-    playIcon.src = "./img/volume_up.png";
+    playIcon.src = "./img/volume_off.png";
     playIcon.style.transform = "rotate(0deg)";
   }
 }
